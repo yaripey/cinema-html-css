a0cdbeb5959fa87039d5938d71537862f2868b86
yaripey <17trubin@gmail.com>
2021-02-18 22:31:25 +0200

    fixed destination

--- js/pages/pageSwitcher.js
+++ js/pages/pageSwitcher.js
@@ -1,7 +1,8 @@
 import pageInit from './pageInit.js'
 
+const url = 'cinema-html-css/'
 
 export default function pageSwitcher(destination) {
-  window.history.pushState(null, null, destination)
+  window.history.pushState(null, null, url + destination)
   pageInit();
 }
